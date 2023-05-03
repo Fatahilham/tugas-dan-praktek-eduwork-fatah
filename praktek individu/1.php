18f6dcb1342620043e1769fc42ba7ddd9a439c86
Fatahilham <fatahamantubillah1@gmail.com>
2023-05-03 19:35:15 +0700

    praktek individu

---
+++ praktek individu/1.php
@@ -0,0 +1,10 @@
+<?php
+
+for ($i = 1; $i <= 9; $i++) {
+    if ($i % 2 == 0) {
+      echo $i . " adalah bilangan genap<br>";
+    } else {
+      echo $i . " adalah bilangan ganjil<br>";
+    }
+  }
+?>  
---
+++ praktek individu/2.php
@@ -0,0 +1,10 @@
+<?php
+//looping for
+for ($i = 0; $i <= 8; $i++) {
+    $str = '';
+    for ($j = 0; $j <= $i; $j++) {
+      $str .= $j;
+    }
+    echo $str . "<br>";
+  }
+?>
